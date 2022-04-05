de76a5526f06dcfebb3099f555e501c534623221
Antonio Bove <antoniofbove@gmail.com>
2022-04-05 21:59:11 +0200

    - input of missing parragraphs in $faqs

--- index.php
+++ index.php
@@ -58,16 +58,28 @@ $faqs = [
     </p>  '
   ],
   [
-    'question' => "",
-    'answer' => ""
+    'question' => "Determining the country associated with your account",
+    'answer' => '<p>
+    When you create a new account, we associate your account with a country based on where you created your Google Account. For accounts at least a year old, we use the country from which you usually access Google services — typically where you\'ve spent the most time in the last year.
+    </p>
+    <p>
+    Frequent travel doesn\'t generally affect the country associated with your account. If you move to a new country, it can take about a year for your country association to update.
+    </p>
+    <p>
+    If the country associated with your account doesn\'t correspond to your country of residence, it could be because of a difference between your country of work and residence, because you\'ve installed a Virtual Private Network (VPN) to mask your IP address, or because you live close to a territorial border. <a href="#">Contact us</a> if you think your country association is wrong.
+    </p>'
   ],
   [
-    'question' => "",
-    'answer' => ""
+    'question' => "How can I remove information about myself from Google's search results?",
+    'answer' => '<p>
+    Google search results are a reflection of the content publicly available on the web. Search engines can\'t remove content directly from websites, so removing search results from Google wouldn\'t remove the content from the web. If you want to remove something from the web, you should <a href="#">contact the webmaster</a> of the site the content is posted on and ask him or her to make a change. Additionally, if under European data protection law, you would like to request removal of certain information about you that appears in Google\'s search results, please <a href="#">click here</a>. Once the content has been removed and Google has noted the update, the information will no longer appear in Google\'s search results. If you have an urgent removal request, you can also <a href="#">visit our help page for more information</a>.
+    </p>'
   ],
   [
-    'question' => "",
-    'answer' => ""
+    'question' => "Are my search queries sent to websites when I click on Google Search results?",
+    'answer' => '<p>
+    In some cases, yes. When you click on a search result in Google Search, your web browser also may send the Internet address, or URL, of the search results page to the destination webpage as the <a href="#">Referrer URL</a>. The URL of the search results page may sometimes contain the search query you entered. If you are using SSL Search (Google\'s encrypted search functionality), under most circumstances, your search terms will not be sent as part of the URL in the Referrer URL. There are some exceptions to this behavior, such as if you are using some less popular browsers. More information on SSL Search can be found <a href="#">here</a>. Search queries or information contained in the Referrer URL may be available via Google Analytics or an application programming interface (API). In addition, advertisers may receive information relating to the exact keywords that triggered an ad click.
+    </p>'
   ],
 ];
 
@@ -107,6 +119,14 @@ $nav_items = [
 </head>
 <body>
   <header>
+    <div class="google-section">
+      <div class="left">
+        <span class="logo"></span><span class="title">Privacy & Terms</span>
+      </div>
+      <div class="right">
+        
+      </div>
+    </div>
     <nav>
       <ul>
         <?php foreach($nav_items as $item):
